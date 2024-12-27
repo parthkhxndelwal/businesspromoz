f9cc250f8c8aaff7629bf6d7e4066543e5557421
Deepanshu Negi <parthmethi@gmail.com>
2024-12-28 03:58:34 +0530

    Release 1.01

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React from 'react';
-import Link from 'next/link';
 import Image from 'next/image';
 import ServicesSection from "@/components/Services";
 import ContactSection from '@/components/Contact';
